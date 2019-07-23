fafedfeb3f725057c974357582791a364b489867
XIAOZUO@qq.com <xiaozuo@qq.com>
2019-07-24 00:10:40 +0800

    商品列表页实现。

--- FreshShop/FreshApp/urls.py
+++ FreshShop/FreshApp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.urls import re_path
 from FreshApp.views import *
 
 urlpatterns = [
     path("register/",register),
     path("login/",login),
     path("index/",index),
+    path("base/",base),
+    path("register_store/",register_store),
+    path("add_goods/",add_goods),
+    path("list_goods/",list_goods),
 ]
--- FreshShop/FreshApp/views.py
+++ FreshShop/FreshApp/views.py
@@ -1,7 +1,8 @@
 import hashlib
-from FreshApp.models import *
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
+from FreshApp.models import *
 
 # Create your views here.
 #对用户的密码进行加密
@@ -59,10 +60,123 @@ def login(request):
                     response = HttpResponseRedirect("/fresh/index")
                     response.set_cookie("username",username)
                     request.session["username"] = username
+                    response.set_cookie("user_id",user.id)#cookie提供用户id方便其他功能查询
                     return response
     return response
 
 @loginVaild
 def index(request):
+    """
+    检查账号是否有店铺
+    """
+    #查询当前用户是谁
+    user_id = request.COOKIES.get("user_id")
+    if user_id:
+        user_id = int(user_id)
+    else:
+        user_id = 0
+    #通过用户查询店铺是否存在(店铺和用户之间用id进行关联)
+    store = Store.objects.filter(user_id=user_id).first()
+    if store:
+        is_store = 1
+    else:
+        is_store = 0
+    return render(request,"freshApp/index.html",{"is_store":is_store})
 
-    return render(request,"freshApp/index.html",locals())
+#base页
+def base(request):
+    return render(request,"freshApp/base.html",locals())
+
+def register_store(request):
+    type_list = StoreType.objects.all()
+    if request.method == "POST":
+        post_data = request.POST
+        store_name = post_data.get("store_name")
+        store_address = post_data.get("store_address")
+        store_descripton = post_data.get("store_descripton")
+        store_phone = post_data.get("store_phone")
+        store_money = post_data.get("store_money")
+
+        user_id = int(request.COOKIES.get("user_id"))
+        type_lists = post_data.getlist("type")
+        store_logo = request.FILES.get("store_logo")
+        store = Store()
+        store.store_name = store_name
+        store.store_address = store_address
+        store.store_descripton = store_descripton
+        store.store_phone = store_phone
+        store.store_money = store_money
+        store.user_id = user_id
+        store.store_logo = store_logo
+        store.save()
+
+        for i in type_lists:
+            store_type = StoreType.objects.get(id=i)
+            store.type.add(store_type)
+        store.save()
+    return render(request,"freshApp/register_store.html",locals())
+
+
+def add_goods(request):
+    """
+    负责添加商品
+    """
+    if request.method == "POST":
+        goods_name = request.POST.get("goods_name")
+        goods_price = request.POST.get("goods_price")
+        goods_number = request.POST.get("goods_number")
+        goods_description = request.POST.get("goods_description")
+        goods_date = request.POST.get("goods_date")
+        goods_safeDate = request.POST.get("goods_safeDate")
+        goods_store = request.POST.get("goods_store")
+        goods_image = request.FILES.get("goods_image")
+        #开始保存数据
+        goods = Goods()
+        goods.goods_name = goods_name
+        goods.goods_price = goods_price
+        goods.goods_number = goods_number
+        goods.goods_description = goods_description
+        goods.goods_date = goods_date
+        goods.goods_safeDate = goods_safeDate
+        goods.goods_image = goods_image
+        goods.save()
+
+        #保存多对多数据
+        goods.store_id.add(
+            Store.objects.get(id=int(goods_store))
+        )
+        goods.save()
+    return render(request,"freshApp/add_goods.html",locals())
+
+#商品以形式展开列表
+def list_goods(request):
+
+    #判断如果搜索不为空的情况下
+    keywords = request.GET.get("keywords","")
+    page_unm = request.GET.get("page_unm",1)
+   #返回上一页的地址和关键字
+    referer = request.META.get("'HTTP_REFERER'")
+    if keywords:
+        goods_list = Goods.objects.filter(goods_name__contains=keywords)
+    else:
+        if referer and "?" in referer:
+            get_str = referer.split("?")[1]
+            get_list = [i.split("=") for i in get_str.split("&")]
+            get_dict = dict(get_list)
+            if "keyword" in get_dict:
+                keywords = get_dict["keywords"]
+            goods_list = Goods.objects.filter(goods_name__contains=keywords)
+        else:
+            goods_list = Goods.objects.all()
+
+    # if keywords:
+    #     #完成的模糊查询
+    #     if keywords in referer:
+    #         goods_list = Goods.objects.filter(goods_name__contains=keywords)
+    # else:
+    #     #如果没有的话返回所有的列表
+    #     goods_list = Goods.objects.all()
+    paginator = Paginator(goods_list,3)
+    page = paginator.page(int(page_unm))
+    page_range = paginator.page_range
+    return render(request,"freshApp/list_goods.html",locals())
