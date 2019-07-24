a95858f9edbf13a5b35f9a71f6dbff174ecfd238
XIAOZUO@qq.com <xiaozuo@qq.com>
2019-07-24 22:47:44 +0800

    用户店铺和cookie校验

--- FreshShop/FreshApp/views.py
+++ FreshShop/FreshApp/views.py
@@ -17,8 +17,6 @@ def loginVaild(fun):
         u_cookies = request.COOKIES.get("username")
         s_session = request.session.get("username")
         if u_cookies and s_session and u_cookies == s_session:
-            user = Seller.objects.filter(username=u_cookies).first()
-            if user:
                 return fun(request,*args,**kwargs)
         return HttpResponseRedirect('/fresh/login')
     return inner
@@ -52,15 +50,23 @@ def login(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         if username and password:
+            # 校验的是用户名是否存在
             user = Seller.objects.filter(username=username).first()
             if user:
                 web_password = setPassword(password)
+                # 校验请求是否来源于登陆页面
                 cookies = request.COOKIES.get("login_form")
+                # 校验密码是否正确
                 if user.password == web_password and cookies == "login_page":
                     response = HttpResponseRedirect("/fresh/index")
                     response.set_cookie("username",username)
                     request.session["username"] = username
                     response.set_cookie("user_id",user.id)#cookie提供用户id方便其他功能查询
+                    store = Store.objects.filter(user_id=user.id).first() #查看店铺是否存在
+                    if store:
+                        response.set_cookie("has_store",store.id).first()
+                    else:
+                        response.set_cookie("has_store","")#检验是否有店铺
                     return response
     return response
 
@@ -70,53 +76,57 @@ def index(request):
     检查账号是否有店铺
     """
     #查询当前用户是谁
-    user_id = request.COOKIES.get("user_id")
-    if user_id:
-        user_id = int(user_id)
-    else:
-        user_id = 0
-    #通过用户查询店铺是否存在(店铺和用户之间用id进行关联)
-    store = Store.objects.filter(user_id=user_id).first()
-    if store:
-        is_store = 1
-    else:
-        is_store = 0
-    return render(request,"freshApp/index.html",{"is_store":is_store})
+    # user_id = request.COOKIES.get("user_id")
+    # if user_id:
+    #     user_id = int(user_id)
+    # else:
+    #     user_id = 0
+    # #通过用户查询店铺是否存在(店铺和用户之间用id进行关联)
+    # store = Store.objects.filter(user_id=user_id).first()
+    # if store:
+    #     is_store = 1
+    # else:
+    #     is_store = 0
+    return render(request,"freshApp/index.html")
 
 #base页
 def base(request):
     return render(request,"freshApp/base.html",locals())
-
+@loginVaild
 def register_store(request):
     type_list = StoreType.objects.all()
     if request.method == "POST":
-        post_data = request.POST
+        post_data = request.POST#接收post请求
         store_name = post_data.get("store_name")
         store_address = post_data.get("store_address")
         store_descripton = post_data.get("store_descripton")
         store_phone = post_data.get("store_phone")
         store_money = post_data.get("store_money")
 
-        user_id = int(request.COOKIES.get("user_id"))
-        type_lists = post_data.getlist("type")
-        store_logo = request.FILES.get("store_logo")
-        store = Store()
+        user_id = int(request.COOKIES.get("user_id"))#通过cookie来得到user_id
+        type_lists = post_data.getlist("type")#通过request.post得到类型，但是是一个列表
+        store_logo = request.FILES.get("store_logo") #通过request.FILES得到
+
+        store = Store()#保存多对多数据
         store.store_name = store_name
         store.store_address = store_address
         store.store_descripton = store_descripton
         store.store_phone = store_phone
         store.store_money = store_money
         store.user_id = user_id
-        store.store_logo = store_logo
-        store.save()
+        store.store_logo = store_logo#django1.8之后图片可以直接保存
+        store.save()#保存，生成了数据库当中的一条数据
 
-        for i in type_lists:
-            store_type = StoreType.objects.get(id=i)
+        for i in type_lists: # 在生成的数据当中添加多对多字段。
+            store_type = StoreType.objects.get(id=i)#循环type列表，得到类型id
             store.type.add(store_type)
-        store.save()
+        store.save()#保存数据
+        response = HttpResponseRedirect("/fresh/index/")
+        response.set_cookie("has_store",store.id)
+        return response
     return render(request,"freshApp/register_store.html",locals())
 
-
+@loginVaild
 def add_goods(request):
     """
     负责添加商品
@@ -128,7 +138,7 @@ def add_goods(request):
         goods_description = request.POST.get("goods_description")
         goods_date = request.POST.get("goods_date")
         goods_safeDate = request.POST.get("goods_safeDate")
-        goods_store = request.POST.get("goods_store")
+        goods_store = request.COOKIES.get("has_store")
         goods_image = request.FILES.get("goods_image")
         #开始保存数据
         goods = Goods()
@@ -146,15 +156,19 @@ def add_goods(request):
             Store.objects.get(id=int(goods_store))
         )
         goods.save()
+        return HttpResponseRedirect("/fresh/list_goods/")
     return render(request,"freshApp/add_goods.html",locals())
 
 #商品以形式展开列表
+@loginVaild
 def list_goods(request):
 
     #判断如果搜索不为空的情况下
-    keywords = request.GET.get("keywords","")
-    page_unm = request.GET.get("page_unm",1)
-   #返回上一页的地址和关键字
+    keywords = request.GET.get("keywords","")#获取关键字
+    page_unm = request.GET.get("page_unm",1)#获取页码
+   #查询店铺
+    store_id = request.COOKIES.get("has_store")
+    store = Store.objects.get(id=int(store_id))
     referer = request.META.get("'HTTP_REFERER'")
     if keywords:
         goods_list = Goods.objects.filter(goods_name__contains=keywords)
@@ -169,14 +183,55 @@ def list_goods(request):
         else:
             goods_list = Goods.objects.all()
 
-    # if keywords:
-    #     #完成的模糊查询
-    #     if keywords in referer:
-    #         goods_list = Goods.objects.filter(goods_name__contains=keywords)
-    # else:
-    #     #如果没有的话返回所有的列表
-    #     goods_list = Goods.objects.all()
     paginator = Paginator(goods_list,3)
     page = paginator.page(int(page_unm))
     page_range = paginator.page_range
     return render(request,"freshApp/list_goods.html",locals())
+
+#设置详情页
+@loginVaild
+def goods(request,goods_id):
+    goods_data = Goods.objects.filter(id=goods_id).first()
+    return render(request,"freshApp/goods.html",locals())
+@loginVaild
+def update_goods(request,goods_id):
+    goods_data = Goods.objects.filter(id=goods_id).first()
+    if request.method == "POST":
+        #获取post请求
+        goods_name = request.POST.get("goods_name")
+        goods_price = request.POST.get("goods_price")
+        goods_number = request.POST.get("goods_number")
+        goods_description = request.POST.get("goods_description")
+        goods_date = request.POST.get("goods_date")
+        goods_safeDate = request.POST.get("goods_safeDate")
+        goods_image = request.FILES.get("goods_image")
+        # 开始修改数据
+        goods = Goods.objects.get(id = int(goods_id))#获取当前商品
+        goods.goods_name = goods_name
+        goods.goods_price = goods_price
+        goods.goods_number = goods_number
+        goods.goods_description = goods_description
+        goods.goods_date = goods_date
+        goods.goods_safeDate = goods_safeDate
+        if goods_image:#如果有上传图片发起修改
+            goods.goods_image = goods_image
+        goods.save()
+        return HttpResponseRedirect("/fresh/goods/%s/"%goods_id)
+    #保存多对多数据
+    return render(request,"freshApp/update_goods.html",locals())
+
+#查询拥有指定商品的店铺
+def CookieTest(request):
+    # 查询拥有指定商品的所有店铺
+    goods = Goods.objects.get(id = 1)
+    store_list = goods.store_id.all()
+    store_list = goods.store_id.filter()
+    store_list = goods.store_id.get()
+    # 查询指定店铺拥有的所有商品
+    store = Store.objects.get(id=17)
+    store.goods_set.filter()
+    store.goods_set.all()
+
+
+
+
